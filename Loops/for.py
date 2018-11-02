14733c658fb2b277efd2608156023325f88ab5eb
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2018-11-02 03:21:02 -0400

    indentation

--- Loops/for.py
+++ Loops/for.py
@@ -1,8 +1,8 @@
-range(1,11) # make an iterable goes from 1 to 10
+range(1, 11)  # make an iterable goes from 1 to 10
 
 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-for numbers in range(1,1001): # prints 1000 numbers
+for numbers in range(1, 1001):  # prints 1000 numbers
     print(numbers)
 
 
@@ -13,13 +13,13 @@ for number in [1, 2, 3, 4]:
 for letter in "abcd":
     print(letter)
 
-for number in range(1,1001, 2): # going in steps of 2
+for number in range(1, 1001, 2):  # going in steps of 2
     print(number)
 
 
 vowels = 0
 consonants = 0
-    
+
 for letter in "Hello":
     if letter.lower() in "aeiou":
         vowels += 1
@@ -32,13 +32,12 @@ print("There are {} vowels".format(vowels))
 print("There are {} consonants".format(consonants))
 
 
-### part 2
+# part 2
 students = {
-    "male" : ["Tom", "Charlie", "Harry", "Frank"],
-    "female" : ["Sarah", "Huda", "Samantha",
-                "Emily", "Elizabeth"]
+    "male": ["Tom", "Charlie", "Harry", "Frank"],
+    "female": ["Sarah", "Huda", "Samantha",
+               "Emily", "Elizabeth"]
 }
-
 
 # prints all students that have "a" in their name
 for key in students.keys():
